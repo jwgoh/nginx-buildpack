bd31fe2ba8a4f76ff4c615dff09b7961b48b4161
Jun Wan Goh <gohjunwan@gmail.com>
2015-11-24 21:36:52 +0800

    Build nginx with http ssl module

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -11,11 +11,9 @@
 
 NGINX_VERSION=${NGINX_VERSION-1.8.0}
 PCRE_VERSION=${PCRE_VERSION-8.37}
-ZLIB_VERSION=${ZLIB_VERSION-1.2.8}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
-zlib_url=http://zlib.net/zlib-${ZLIB_VERSION}.tar.gz
 
 temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
 
@@ -32,16 +30,12 @@ curl -L $nginx_tarball_url | tar xzv
 echo "Downloading $pcre_tarball_url"
 (cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvj )
 
-echo "Downloading $zlib_url"
-(cd nginx-${NGINX_VERSION} && curl -L $zlib_url | tar xvz )
-
 (
   cd nginx-${NGINX_VERSION}
   ./configure \
     --with-pcre=pcre-${PCRE_VERSION} \
-    --with-zlib=zlib-${ZLIB_VERSION} \
     --prefix=/tmp/nginx \
-    --with-http_gzip_static_module \
+    --with-https_ssl_module \
     --with-cc-opt='-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2' \
     --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,--as-needed'
 
